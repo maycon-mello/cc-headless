b3629bf67282a3133347b20dff7e78babddf6670
maycon-mello <maycon.mello@objectedge.com>
2018-07-01 15:03:05 -0300

    Remove logs on proxy

--- src/proxy/proxyMiddleware.js
+++ src/proxy/proxyMiddleware.js
@@ -10,34 +10,16 @@ export default (props) => proxy({
 
   onProxyReq(proxyReq, req, res) {
     if (Util.isXHR(req)) {
-      // Remove occ headers
       proxyReq.removeHeader('user-agent');  
     }          
 
     proxyReq.removeHeader('referer');
-
-    // connection: 'close',
-    // 'content-length': '3645',
-    // 'accept-encoding': 'gzip, deflate',
-    // cookie: 'JSESSIONID=FoNWyeJLmIO-1QJqodgCacf10mUyfDXDbWUU61XD72GZ05JbzIhj!-687441668',
-    // host: 'ccstore-test-zboa.oracleoutsourcing.com',
-    // accept: '*/*',
-    // 'postman-token': 'a1d543bd-b38a-4d9c-bce9-b3429234de7b',
-    // 'cache-control': 'no-cache',
-    // 'content-type': 'application/json',
-  
-
     proxyReq.removeHeader('origin');
     proxyReq.removeHeader('accept-language');
     proxyReq.removeHeader('pragma');
     proxyReq.setHeader('accept', '*/*');
     proxyReq.setHeader('accept-encoding', 'gzip, deflate');
-    
-    
-    
-
-    console.log(proxyReq.getHeaders());
-
+  
     if (props.onProxyReq) {
       props.onProxyReq(proxyReq, req, res);
     }
